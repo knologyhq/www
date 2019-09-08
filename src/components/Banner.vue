55c4722f0af575f43cbbb037cfa858830a830925
Jennie Robinson Faber <jennie@jenniefaber.com>
2019-09-08 17:44:23 -0400

    Minor text styling update

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,7 +3,7 @@
     class="mb-4 pa-12 elevation-6 banner"
     :style="`background-image: linear-gradient(to bottom, rgba(38,96,147,1), rgba(38,96,147,0.577468487394958)), url(${banner.image})`"
   >
-    <h1 v-html="banner.title" class="display-2 white--text text-uppercase mb-4" />
+    <h1 v-html="banner.title" class="display-2 white--text font-weight-black text-uppercase mb-4" />
     <h2 class="title white--text mb-12" v-html="banner.copy" />
     <ReadMoreButton v-if="banner.button == true" />
   </v-sheet>
