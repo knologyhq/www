3f999629b860148f683007c370d882f78e23925a
Jennie Faber <jennie@tomediaarts.org>
2019-09-08 21:04:01 -0400

    Responsive updates for pillar page

--- src/templates/Pillars.vue
+++ src/templates/Pillars.vue
@@ -18,16 +18,18 @@
 
     <v-container fluid>
       <v-row>
-        <v-col cols="2">
+        <v-col cols="12" md="2">
           <div v-html="$page.pillars.svgIcon" />
         </v-col>
-        <v-col cols="6">
+        <v-col cols="12" md="6">
+          <div class="title black--text font-weight-black">Description</div>
+
           <div v-if="$page.pillars.description" v-html="marked($page.pillars.description)" />
         </v-col>
 
-        <v-col cols="4">
+        <v-col cols="12" md="4">
+          <div class="title black--text font-weight-black">Initiatives</div>
           <v-list>
-            <v-subheader>Initiatives</v-subheader>
             <v-list-item-group color="primary">
               <v-list-item v-for="initiative in $page.pillars.initiative" :key="initiative.id">
                 <v-list-item-content>
@@ -45,7 +47,7 @@
       <div class="title text--black font-weight-black">Meet Our Experts in this Research Area</div>
 
       <v-row class="mb-6">
-        <v-col cols="3" v-for="edge in $page.people.belongsTo.edges" :key="edge.node.id">
+        <v-col cols="12" md="3" v-for="edge in $page.people.belongsTo.edges" :key="edge.node.id">
           <PersonCard :person="edge.node" :key="edge.node.id" />
         </v-col>
       </v-row>
@@ -53,7 +55,7 @@
     <v-container v-if="$page.posts.belongsTo.edges">
       <div class="title text--black font-weight-black">Recent Posts About {{$page.pillars.title}}</div>
       <v-row class="mb-6">
-        <v-col cols="3" v-for="edge in $page.posts.belongsTo.edges" :key="edge.node.id">
+        <v-col cols="12" md="3" v-for="edge in $page.posts.belongsTo.edges" :key="edge.node.id">
           <PostCardLarge :post="edge.node" :key="edge.node.id" />
         </v-col>
       </v-row>
