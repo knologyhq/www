6891e24f6f3db79e1f5087fe5a20ad8259fee114
Jennie Faber <jennie@tomediaarts.org>
2019-09-09 02:12:09 -0400

    Style banner text with markdown

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,7 +4,7 @@
     :style="`background-image: linear-gradient(to bottom, rgba(38,96,147,1), rgba(38,96,147,0.577468487394958)), url(${banner.image})`"
   >
     <h1 v-html="banner.title" class="display-2 white--text font-weight-black text-uppercase mb-4" />
-    <h2 class="title white--text mb-12" v-html="banner.copy" />
+    <div class="title white--text mb-12" v-html="marked(banner.copy)" />
     <ReadMoreButton v-if="banner.button == true" />
   </v-sheet>
 </template>
