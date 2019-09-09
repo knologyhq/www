af5b49c36d9918e3dbd0575a0c09de387fc35ba2
Jennie Faber <jennie@tomediaarts.org>
2019-09-09 14:13:58 -0400

    responsive size for banner titles

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,7 +3,10 @@
     class="mb-4 pa-12 elevation-6 banner"
     :style="`background-image: linear-gradient(to bottom, rgba(38,96,147,1), rgba(38,96,147,0.577468487394958)), url(${banner.image})`"
   >
-    <h1 v-html="banner.title" class="display-2 white--text font-weight-black text-uppercase mb-4" />
+    <h1
+      v-html="banner.title"
+      :class="`white--text font-weight-black text-uppercase mb-4 ${$vuetify.breakpoint.smAndDown ? 'subtitle' : 'display-2'}`"
+    />
     <div class="title white--text mb-12" v-html="marked(banner.copy)" />
     <ReadMoreButton v-if="banner.button == true" />
   </v-sheet>
