9ed46defcc76833cc701dd24d63d152fac8ae8bb
Bill Levay <wjlevay@gmail.com>
2023-08-08 13:36:32 -0400

    attempt to update commenting js

--- src/lambda/comment-action.js
+++ src/lambda/comment-action.js
@@ -1,87 +1,80 @@
 "use strict";
 
-var request = require("request");
-console.log(request);
+const axios = require("axios");
+const querystring = require("querystring");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// populate environment variables locally.
-require("dotenv").config();
 /*
-  delete this submission via the api
+  Delete this submission via the API
 */
-function purgeComment(id) {
-  var url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${process.env.API_AUTH}`;
-  request.delete(url, function(err, response, body) {
-    if (err) {
-      return console.log(err);
-    } else {
-      return console.log("Comment deleted from queue.");
-    }
-  });
+async function purgeComment(id) {
+  try {
+    const url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${process.env.API_AUTH}`;
+    const response = await axios.delete(url);
+    console.log("Comment deleted from queue.");
+  } catch (error) {
+    console.error("Error deleting comment:", error.message);
+  }
 }
 
 /*
   Handle the lambda invocation
 */
-export function handler(event, context, callback) {
-  console.log("Slack got inside the handler!");
-  // parse the payload
-  var body = event.body.split("payload=")[1];
-  var payload = JSON.parse(unescape(body));
-  var method = payload.actions[0].name;
-  var id = payload.actions[0].value;
+exports.handler = async (event) => {
+  try {
+    console.log("Slack got inside the handler!");
+    // parse the payload
+    const payload = JSON.parse(querystring.parse(event.body).payload);
+    const method = payload.actions[0].name;
+    const id = payload.actions[0].value;
 
-  if (method == "delete") {
-    purgeComment(id);
-    callback(null, {
-      statusCode: 200,
-      body: "Comment deleted"
-    });
-  } else if (method == "approve") {
-    // get the comment data from the queue
-    var url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${process.env.API_AUTH}`;
+    if (method === "delete") {
+      await purgeComment(id);
+      return {
+        statusCode: 200,
+        body: "Comment deleted",
+      };
+    } else if (method === "approve") {
+      // get the comment data from the queue
+      const url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${process.env.API_AUTH}`;
+      const response = await axios.get(url);
 
-    request(url, function(err, response, body) {
-      if (!err && response.statusCode === 200) {
-        var data = JSON.parse(body).data;
+      if (response.status === 200) {
+        const data = response.data.data;
         console.log(data);
 
-        // now we have the data, let's massage it and post it to the approved form
-        var payload = {
+        // prepare data to post to the approved form
+        const approvedPayload = {
           "form-name": "comments-approved",
           path: data.path,
           postId: data.postId,
           received: new Date().toString(),
           last_name: data.last_name,
           first_name: data.first_name,
-          comment: data.comment
+          comment: data.comment,
         };
-        var approvedURL = process.env.URL;
+        const approvedURL = process.env.URL;
 
         console.log("Posting to", approvedURL);
 
-        request.post({ url: approvedURL, formData: payload }, function(
-          err,
-          httpResponse,
-          body
-        ) {
-          var msg;
-          console.log(payload);
-          if (err) {
-            msg = "Post to approved comments failed:" + err;
-            console.log(msg);
-          } else {
-            msg = "Post to approved comments list successful.";
-            console.log(msg);
-            purgeComment(id);
-          }
-          var msg = "Comment registered. Site deploying to include it.";
-          callback(null, {
-            statusCode: 200,
-            body: msg
-          });
-          return console.log(msg);
-        });
+        const approvedResponse = await axios.post(approvedURL, querystring.stringify(approvedPayload));
+        console.log("Post to approved comments:", approvedResponse.data);
+
+        await purgeComment(id);
+
+        const msg = "Comment registered. Site deploying to include it.";
+        return {
+          statusCode: 200,
+          body: msg,
+        };
       }
-    });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "An error occurred while processing the request." }),
+    };
   }
-}
+};
--- src/lambda/comment-handler.js
+++ src/lambda/comment-handler.js
@@ -1,68 +1,57 @@
 "use strict";
 
-var request = require("request");
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// populate environment variables locally.
-require("dotenv").config();
+exports.handler = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
 
-/*
-  Our serverless function handler
-*/
-export function handler(event, context, callback) {
-  // get the arguments from the notification
-  var body = JSON.parse(event.body);
+    const slackPayload = {
+      text: `New comment on ${process.env.URL}`,
+      attachments: [
+        {
+          fallback: "New comment on the Knology web site",
+          color: "#444",
+          author_name: body.data.first_name,
+          title: body.data.path,
+          title_link: `${process.env.URL}${body.data.path}`,
+          text: body.data.comment,
+        },
+        {
+          fallback: `Manage comments on ${process.env.URL}`,
+          callback_id: "comment-action",
+          actions: [
+            {
+              type: "button",
+              text: "Approve comment",
+              name: "approve",
+              value: body.id,
+            },
+            {
+              type: "button",
+              style: "danger",
+              text: "Delete comment",
+              name: "delete",
+              value: body.id,
+            },
+          ],
+        },
+      ],
+    };
 
-  // prepare call to the Slack API
-  var slackURL = process.env.SLACK_WEBHOOK_URL;
-  var slackPayload = {
-    text: "New comment on " + process.env.URL,
-    attachments: [
-      {
-        fallback: "New comment on the Knology web site",
-        color: "#444",
-        author_name: body.data.first_name,
-        title: body.data.path,
-        title_link: process.env.URL + body.data.path,
-        text: body.data.comment
-      },
-      {
-        fallback: "Manage comments on " + process.env.URL,
-        callback_id: "comment-action",
-        actions: [
-          {
-            type: "button",
-            text: "Approve comment",
-            name: "approve",
-            value: body.id
-          },
-          {
-            type: "button",
-            style: "danger",
-            text: "Delete comment",
-            name: "delete",
-            value: body.id
-          }
-        ]
-      }
-    ]
-  };
+    const response = await axios.post(process.env.SLACK_WEBHOOK_URL, slackPayload);
 
-  // post the notification to Slack
-  request.post({ url: slackURL, json: slackPayload }, function(
-    err,
-    httpResponse,
-    body
-  ) {
-    var msg;
-    if (err) {
-      msg = "Post to Slack failed:" + err;
-    } else {
-      msg = "Post to Slack successful!  Server responded with:" + body;
-    }
-    callback(null, {
-      statusCode: 200,
-      body: msg
-    });
-    return console.log(msg);
-  });
-}
+    return {
+      statusCode: response.status,
+      body: JSON.stringify({ message: "Post to Slack successful!", response: response.data }),
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "An error occurred while posting to Slack." }),
+    };
+  }
+};
