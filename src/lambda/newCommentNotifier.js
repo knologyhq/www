ec2578648bd83a6aaca53e5c2701a4c996da2886
Bill Levay <wjlevay@gmail.com>
2023-11-01 15:22:27 -0400

    adding logging to CommentNotifier function

--- src/lambda/newCommentNotifier.js
+++ src/lambda/newCommentNotifier.js
@@ -1,62 +1,64 @@
-"use strict";
-
-const request = require("request");
-require("dotenv").config();
+const axios = require('axios');
+require('dotenv').config();
 
 exports.handler = async function(event, context) {
 	try {
-		const body = JSON.parse(event.body);
-		const slackURL = process.env.SLACK_WEBHOOK_URL;
+		console.log('newCommentNotifier function invoked');
 
+		const body = JSON.parse(event.body);
+
+		// prepare call to the Slack API
+		const slackURL = process.env.SLACK_WEBHOOK_URL;
 		const slackPayload = {
-			text: `New comment on ${process.env.URL}`,
+			text: 'New comment on ' + process.env.URL,
 			attachments: [
 				{
-					fallback: "New comment on the website",
-					color: "#444",
+					fallback: 'New comment',
+					color: '#444',
 					author_name: `${body.data.first_name} ${body.data.last_name}`,
 					title: body.data.path,
-					title_link: `${process.env.URL}${body.data.path}`,
-					text: body.data.comment
+					title_link: process.env.URL + body.data.path,
+					text: body.data.comment,
 				},
 				{
-					fallback: `Manage comments on ${process.env.URL}`,
-					callback_id: "comment-action",
+					fallback: 'Manage comments',
+					callback_id: 'comment-action',
 					actions: [
 						{
-							type: "button",
-							text: "Approve comment",
-							name: "approve",
-							value: body.id
+							type: 'button',
+							text: 'Approve comment',
+							name: 'approve',
+							value: body.id,
 						},
 						{
-							type: "button",
-							style: "danger",
-							text: "Delete comment",
-							name: "delete",
-							value: body.id
-						}
-					]
-				}
-			]
+							type: 'button',
+							style: 'danger',
+							text: 'Delete comment',
+							name: 'delete',
+							value: body.id,
+						},
+					],
+				},
+			],
 		};
 
-		request.post({ url: slackURL, json: slackPayload }, function(err, httpResponse, body) {
-			if (err) {
-				return {
-					statusCode: 500,
-					body: `Failed to post to Slack: ${err}`
-				};
-			}
-		});
+		console.log('Sending payload to Slack: ', JSON.stringify(slackPayload));
+
+		const result = await axios.post(slackURL, slackPayload);
+
+		console.log('Slack API response:', result.data);
+
 		return {
 			statusCode: 200,
-			body: "Notification sent to Slack."
+			body: JSON.stringify({ message: 'Notification sent' }),
 		};
+
 	} catch (error) {
+		console.log('Error:', error);
+
 		return {
 			statusCode: 500,
-			body: `Error: ${error}`
+			body: JSON.stringify({ error: 'Failed to notify' }),
 		};
 	}
 };
