9b9675e70bba2dead928f0f30560560311443f17
Bill Levay <wjlevay@gmail.com>
2024-10-26 14:26:55 -0400

    Add new version of PostCardLarge

---
+++ src/components/PostCardLargeNoDate.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card :class="`mx-auto mb-8 ${postClasses}`" flat tile :to="`/article/${post.slug}`">
+    <v-img
+      v-if="post.imageThumb"
+      height="200px"
+      :src="`${post.imageThumb.url}?auto=compress&w=500&fit=fillmax`"
+    />
+    <v-img
+      v-else="post.image"
+      height="200px"
+      :src="`${post.image.url}?auto=compress&w=500&fit=fillmax`"
+    />
+    <v-card-text class="pa-0 mb-0 mt-2">
+      <span
+        class="category overline darken-2 mb-2"
+        v-for="category in post.categories"
+        :key="category.id"
+        color="grey"
+        v-html="category.title"
+      />
+    </v-card-text>
+    <v-card-title class="pa-0 mb-0 mt-2">
+      <div class="black--text font-weight-black subtitle mb-2">{{ post.title }}</div>
+      <p v-if="post.subtitle" color="grey" class="darken-1 body-1 mb-2">{{ post.subtitle }}</p>
+    </v-card-title>
+  </v-card>
+</template>
+
+<style lang="postcss" scoped>
+.category + .category:before {
+  content: " | ";
+}
+.subtitle {
+  line-height: 1.7rem;
+}
+</style>
+
+<script>
+export default {
+  name: "PostCardLargeNoDate",
+  props: ["post", "postClasses"]
+};
+</script>
+
